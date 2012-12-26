baf23e2787f84c4aae43b183e25438538925f91d
Paul King <paulk@asert.com.au>
2012-12-26 19:23:10 +1000

    spike for GROOVY-5864: @AutoExternalize doesn't create no-argument constructor

--- src/main/groovy/transform/AutoExternalizeVerifier.java
+++ src/main/groovy/transform/AutoExternalizeVerifier.java
@@ -26,61 +26,15 @@ import java.lang.annotation.Target;
  * Note: This annotation is currently experimental! Use at your own risk!
  * <p/>
  * Class annotation used to assist in the creation of {@code Externalizable} classes.
- * The {@code @AutoExternalize} annotation instructs the compiler to execute an
- * AST transformation which adds {@code writeExternal()} and {@code readExternal()} methods
- * to a class and adds {@code Externalizable} to the interfaces which the class implements.
- * The {@code writeExternal()} method writes each property (or field) for the class while the
- * {@code readExternal()} method will read each one back in the same order. Properties or fields
- * marked as {@code transient} are ignored.
- * <p/>
- * Example usage:
- * <pre>
- * import groovy.transform.*
- * {@code @AutoExternalize}
- * class Person {
- *   String first, last
- *   List favItems
- *   Date since
- * }
- * </pre>
- * Which will create a class of the following form:
- * <pre>
- * class Person implements Externalizable {
- *   ...
- *   public void writeExternal(ObjectOutput out) throws IOException {
- *     out.writeObject(first)
- *     out.writeObject(last)
- *     out.writeObject(favItems)
- *     out.writeObject(since)
- *   }
- *
- *   public void readExternal(ObjectInput oin) {
- *     first = oin.readObject()
- *     last = oin.readObject()
- *     favItems = oin.readObject()
- *     since = oin.readObject()
- *   }
- *   ...
- * }
- * </pre>
+ * The {@code @AutoExternalizeVerifier} annotation instructs the compiler to check
+ * that all expected conditions required for the {@code AutoExternalize} transform
+ * have been met.
  *
  * @author Paul King
- * @since 1.8.0
+ * @since 2.1.0
  */
 @java.lang.annotation.Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @GroovyASTTransformationClass("org.codehaus.groovy.transform.AutoExternalizeVerifierASTTransformation")
-public @interface AutoExternalizeVerifier {
-    /**
-     * Comma separated list of property names to exclude from externalizing.
-     * For convenience, a String with comma separated names
-     * can be used in addition to an array (using Groovy's literal list notation) of String values.
-     */
-    String[] excludes() default {};
-
-    /**
-     * Include fields as well as properties when externalizing.
-     */
-    boolean includeFields() default false;
-}
+public @interface AutoExternalizeVerifier { }
